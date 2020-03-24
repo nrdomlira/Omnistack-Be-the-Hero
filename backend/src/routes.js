9c9c93aae3b3b88f68c56e0977e1f4d3a09ae3ee
José Domingos <domliraa@gmail.com>
2020-03-24 12:46:09 -0300

    feat: create backend application

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
+const ONGController = require('../controllers/ONGController');
+const IncidentController = require('../controllers/IncidentController');
+const ProfileController = require('../controllers/ProfileController');
+const SessionController = require('../controllers/SessionController');
+
 const router = express.Router();
 
+router.post('/sessions', SessionController.store);
 
-router.get('/', (req, res) =>{
-    res.json({ok: "teste"})
-});
+
+router.get('/ong', ONGController.index);
+router.post('/ong', ONGController.store);
+
+router.get('/incidents', IncidentController.index);
+router.post('/incidents', IncidentController.store);
+router.delete('/incidents/:id', IncidentController.delete);
+
+router.get('/profile', ProfileController.index);
 
 module.exports = router;
